b1be1d8acec50a7adc8f4076ba2b169066b14563
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-02-25 13:20:13 +0530

    25-02-2026 all data uploaded

---
+++ module-3-core-php/practical/52-string.php
@@ -0,0 +1,23 @@
+<?php 
+/* 
+ what is string :
+ A string is a set of characters i.e called string 
+ A string enclosed within '' or "" quotes 
+ A string is an data types of variables 
+
+ examples :
+
+
+ $name="brijesh pandey";
+ $str='hi i am brijesh';
+ $str1="15451201";
+
+*/
+
+
+ $name="brijesh pandey";
+ $str='hi i am brijesh';
+ $str1="15451201";
+ echo $name."<br>".$str."<br>".$str1;
+
+?>
